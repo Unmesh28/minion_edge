c8309e60fdb3d8f02d67cb5d533f3fa90b03d2e7
Unmesh28 <unmesh.dabhade@gmail.com>
2022-02-16 12:41:10 +0530

    first commit

---
+++ procees_push_cloud.py
@@ -0,0 +1,96 @@
+from distutils.command.clean import clean
+import numpy as np
+from sklearn import preprocessing
+import tensorflow as tf
+
+def clean_voltage(voltage) :
+    voltage_data = str(voltage).split('.')
+    if len(voltage_data) > 1:
+        res = float(voltage_data[0]+"."+voltage_data[1])
+    else:
+        res = float(voltage_data[0])
+    return res
+
+def clean_current(current) :
+    current_data = str(current).split('.')
+    if len(current_data) > 1:
+        res = float(current_data[0]+"."+current_data[1])
+    else:
+        res = float(current_data[0])
+    return res
+
+def clean_realPower(realPower):
+    rp_data = str(realPower).split(".")
+    if len(rp_data) > 1:
+        res = float(rp_data[0]+"."+rp_data[1])
+    else:
+        res = float(rp_data[0])
+    return res
+
+def clean_powerFactor(powerFactor):
+    pf_data = str(powerFactor).split(".")
+    if len(pf_data) > 1:
+        res = int(pf_data[0]+"."+pf_data[1])
+    else:
+        res = int(pf_data[0])
+    return res
+
+def clean_reactive_power(reactivePower):
+    rp_data = str(reactivePower).split(".")
+    if len(rp_data) > 1:
+        res = float(rp_data[0]+"."+rp_data[1])
+    else:
+        res = float(rp_data[0])
+    return res
+
+
+def clean_frequency(frequency) :
+    freq_data = str(frequency).split(".")
+    if len(freq_data) > 1:
+        res = float(freq_data[0]+"."+freq_data[1])
+    else:
+        res = float(freq_data[0])
+    return res
+
+
+def process_input(data) :
+    X = []
+    X.append(clean_voltage(data[1]))  
+    X.append(clean_current[data[2]])
+    X.append(clean_realPower(data[3]))
+    X.append(clean_powerFactor(data[4]))
+    X.append(clean_reactive_power(data[6]))
+    X.append(clean_frequency(data[7]))
+    print(X)
+    get_model_result(X)
+
+def get_model_result(X):
+    quantile_transformer = preprocessing.QuantileTransformer(random_state=0)
+
+    X_tansform = quantile_transformer.fit_transform(X)
+
+    print(X_tansform)
+    interpreter = tf.lite.Interpreter(model_path="/home/pi/minion_lab/model.tflite")
+    print(interpreter.get_input_details())
+    print(interpreter.get_output_details())
+    print(interpreter.get_tensor_details())
+    interpreter.allocate_tensors()
+
+    # Get input and output tensors.
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+    print(input_details)
+
+    # Test the model on input data.
+    input_shape = input_details[0]['shape']
+    print(input_shape)
+
+    input_data = np.array(X_tansform, dtype=np.float32)
+    input_data = np.array(X_tansform.reshape(1,7), dtype=np.float32)
+
+    interpreter.set_tensor(input_details[0]['index'], input_data)
+
+    interpreter.invoke()
+
+    output_data = interpreter.get_tensor(output_details[0]['index'])
+    print("Output data :" + str(output_data))
---
+++ single_phase.py
@@ -0,0 +1,55 @@
+import serial
+import datetime
+from time import sleep
+import RPi.GPIO as GPIO
+from procees_push_cloud import process_input
+from multiprocessing import Process
+
+GPIO.setwarnings(False)
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(4, GPIO.OUT, initial=GPIO.HIGH)
+
+GPIO.output(4, GPIO.LOW)
+sleep(0.005)
+GPIO.output(4, GPIO.HIGH)
+
+ser = serial.Serial('/dev/ttyUSB0',115200,timeout = 1 , parity = serial.PARITY_NONE, rtscts = 0)
+ser.flush()
+countr = 0
+head = 'TimeStamp,Voltage,Current,RealPower,PowerFactor,ApparantPower,Reactivepower,Frequency'
+
+with open('/home/pi/single_phase_log.csv','a') as fp:
+    fp.write(head+'\n')
+fp.close()
+
+def write_to_csv(data) :
+    with open('/home/pi/single_phase_log.csv','a') as fp:
+            fp.write(data)
+        #fp.close()
+
+
+while True:
+    
+    t = datetime.datetime.now()
+    ct = str(t)
+
+    try:
+        line = ser.readline()
+        print(line)
+        line1 = line.decode()
+        data = ct+','+line1 
+        data1 = data.split(',')
+        print(line1)
+        p1 = Process(target=process_input, args=(data1))
+        p2 = Process(target=write_to_csv, args=(data))
+        p1.start()
+        p2.start()
+        p1.join()
+        p2.join()
+        
+
+    except Exception as e:
+        fp.close()
+        #print(e)
